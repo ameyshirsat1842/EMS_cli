00756cf949dcc36ae1bb0947ef28e1a6f281756a
ameyshirsat1842 <ameyshirsat1842@gmail.com>
2024-06-06 10:58:48 +0530

    Initial commit

---
+++ db_config.py
@@ -0,0 +1,4 @@
+config = {
+    'user' : 'user_name'
+
+}
---
+++ db_connection.py
@@ -0,0 +1,9 @@
+import _mysql_connector
+from _mysql_connector import errorcode
+from config import config
+
+print("## Employee Management System by Amey ##")
+print("You can use this app to Add, Update, View or Delete employees from your Database")
+
+
+
